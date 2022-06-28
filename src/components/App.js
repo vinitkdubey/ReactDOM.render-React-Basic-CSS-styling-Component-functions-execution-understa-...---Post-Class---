bf29209b9ed4e355d591d3d5b4169a6cfb00d422
vinitkdubey <100983850+vinitkdubey@users.noreply.github.com>
2022-06-28 13:22:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
+import React from 'react';
 import '../styles/App.css';
 
+//! App is the name of component as per the test
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
+    <p>
+      Now I can render any React component on any DOM node I want using
+      ReactDOM.render
+    </p>
+  );
+};
 
-
+//! Needed so that `index.js` can do `import App from './App.js'`
 export default App;
