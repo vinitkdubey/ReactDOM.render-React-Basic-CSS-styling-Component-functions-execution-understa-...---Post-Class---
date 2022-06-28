9cd75154c5258e78c130d450d6ccb6e6518ba703
vinitkdubey <100983850+vinitkdubey@users.noreply.github.com>
2022-06-28 13:14:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './index.css';
 import App from "./components/App";
 
-
+const App = () => {
+  return (
+    <div>  <App/></div>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
