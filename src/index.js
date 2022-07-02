bc38adb6874b4d9c5f01fe123614826a954fdb9f
vinitkdubey <100983850+vinitkdubey@users.noreply.github.com>
2022-07-02 09:46:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js'
+// import App from './App.js'
+ import App from "./components/App";
 
 ReactDOM.render(
   <App />,
